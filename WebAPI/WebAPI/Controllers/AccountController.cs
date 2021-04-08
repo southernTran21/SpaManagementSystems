bd62b46e6e02ec53980c562cc018a60eaa791c56
southernTran21 <tranphuongnam21.06.99@gmail.com>
2021-04-08 15:43:52 +0700

    done setup API and create example API Account

---
+++ WebAPI/WebAPI/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        
+        public AccountController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"SELECT [id] ,[username] ,[password] ,[accountName] ,[idQuyen] FROM [dbo].[Account]";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DBConnection");
+            SqlDataReader myreader;
+            using (SqlConnection myCon= new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using(SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myreader = myCommand.ExecuteReader();
+                    table.Load(myreader);
+                    myreader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+    
+    
+    
+    }
+}
