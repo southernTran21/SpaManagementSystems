838f43415c697bdd2cfa25c2d33ea417b382ab1d
southernTran21 <tranphuongnam21.06.99@gmail.com>
2021-04-09 23:05:15 +0700

    commit UI login

--- web-ui/src/layout/login/index.js
+++ web-ui/src/layout/login/index.js
@@ -1,10 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Input } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+
+import logoSpa from "../../image/logo-spa-12.jpg";
+import waveUp from "../../image/wave.svg";
+import waveDow from "../../image/wave-dow.svg";
 
 export default class Login extends Component {
     render() {
         return (
             <div className="login">
+                <div className="login-wave-dow">
+                    <img src={waveDow} alt="" />
+                </div>
+
+                <div className="login-form">
+                    {/* <div className="login-logo">
+                        <img src={logoSpa} alt="" />
+                    </div> */}
+                    <div className="login-title">LOGIN</div>
+                    <div className="login-input">
+                        <UserOutlined />
+                        <Input placeholder="Tên Đăng Nhập" />
+                    </div>
+                    <div className="login-input">
+                        <LockOutlined />
+                        <Input placeholder="Mật Khẩu" />
+                    </div>
+                    <div className="login-button-submit">Đăng Nhập</div>
+                </div>
+                <div className="login-wave-up">
+                    <img src={waveUp} alt="" />
+                </div>
             </div>
-        )
+        );
     }
 }
